6cefcc05ae2cd6930a87c49d7da4b1843073296f
sajjad-njr <maxsajjad29@gmail.com>
2023-10-05 23:26:50 +0600

    text animation left right up down added

--- lib/Screens/menuscreen.dart
+++ lib/Screens/menuscreen.dart
@@ -5,15 +5,17 @@ import 'package:flutter_animations/Colors/colors.dart';
 import 'package:get/get.dart';
 import '../Controllers/drawercontroller.dart';
 
-
 class MenuScreen extends GetView<MyDrawerController> {
   final Function(int) onMenuItemClicked;
-
 
   MenuScreen({required this.onMenuItemClicked});
 
   // Menu items for illustration
-  final List<String> menuItems = ['Guidelines', 'Fade in/out'];
+  final List<String> menuItems = [
+    'Guidelines',
+    'Fade in/out',
+    'Text Animation'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,23 +24,26 @@ class MenuScreen extends GetView<MyDrawerController> {
         color: menupagecolor,
         child: Column(
           children: [
-            Text("Menu",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.white,
-            ),),
+            Text(
+              "Menu",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
             SizedBox(height: 40),
             Expanded(
               child: ListView.builder(
                 itemCount: menuItems.length,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text(menuItems[index],
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
+                    title: Text(
+                      menuItems[index],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
                     ),
                     onTap: () {
                       // Call onMenuItemClicked when an item is clicked
