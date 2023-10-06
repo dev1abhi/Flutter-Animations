93fe25623727827d2a653beeef4070793822b087
Bidur Gupta <bidurgupta125@gmail.com>
2023-10-06 16:11:52 +0530

    Added Flutter water drop effect

--- lib/Screens/water_drop_effect.dart
+++ lib/Screens/water_drop_effect.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class WaterDropEffect extends StatelessWidget {
-  const WaterDropEffect({super.key});
+  const WaterDropEffect({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,118 +9,111 @@ class WaterDropEffect extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            const  Spacer(),
-            // Custom Nuemorphic Container
+            const Spacer(),
+            // Custom Neumorphic Container
             Center(
-              child:
-              Container(height: 180,
+              child: Container(
+                height: 180,
                 width: 180,
-
-                decoration:const  BoxDecoration(
+                decoration: const BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.green,
-
                 ),
                 alignment: Alignment.center,
                 child: Container(
                   height: 150,
                   width: 150,
-                  foregroundDecoration:const  BoxDecoration(
-                      shape: BoxShape.circle,
-                      //color: Colors.white
-                      backgroundBlendMode: BlendMode.overlay,
-                      gradient: LinearGradient(
-
-                          colors: [Colors.black,Colors.white]
-                      )
+                  foregroundDecoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    // color: Colors.white
+                    backgroundBlendMode: BlendMode.overlay,
+                    gradient: LinearGradient(
+                      colors: [Colors.black, Colors.white],
+                    ),
                   ),
                 ),
               ),
             ),
-            //END
-            const  Spacer(),
-            // Custom Nuemorphic Container button
+            // END
+            const Spacer(),
+            // Custom Neumorphic Container button
             Center(
-              child:
-              Container(height: 165,
+              child: Container(
+                height: 165,
                 width: 165,
-
-                decoration:const  BoxDecoration(
+                decoration: const BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.green,
-
                 ),
                 alignment: Alignment.center,
                 child: Container(
                   height: 150,
                   width: 150,
-                  foregroundDecoration:const  BoxDecoration(
-                      shape: BoxShape.circle,
-                      //color: Colors.white
-                      backgroundBlendMode: BlendMode.overlay,
-                      gradient: LinearGradient(
-
-                          colors: [Colors.black,Colors.white]
-                      )
+                  foregroundDecoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    // color: Colors.white
+                    backgroundBlendMode: BlendMode.overlay,
+                    gradient: LinearGradient(
+                      colors: [Colors.black, Colors.white],
+                    ),
                   ),
                 ),
               ),
             ),
-            //END
-            const  Spacer(),
-
+            // END
+            const Spacer(),
             // Custom Water drop
             Center(
-              child:
-              Stack(
+              child: Stack(
                 alignment: Alignment.center,
                 children: [
-                  Container(height: 200,
+                  Container(
+                    height: 200,
                     width: 200,
                     color: Colors.green,
-
                     alignment: Alignment.center,
-
-
                   ),
                   Container(
                     height: 150,
                     width: 150,
                     decoration: BoxDecoration(
-                        color: Colors.green,
-                        shape: BoxShape.circle,
-
-                        boxShadow: [
-                          BoxShadow(color: Colors.black.withOpacity(0.5),blurRadius: 5,offset: const Offset(5,6))]
-
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 5,
+                          offset: const Offset(5, 6),
+                        ),
+                      ],
                     ),
-
-
                   ),
-
-
                   Stack(
-
                     children: [
                       Container(
-                        margin: const  EdgeInsets.only(top: 40,left: 25),
+                        margin: const EdgeInsets.only(top: 40, left: 25),
                         height: 20,
                         width: 20,
                         decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(color: Colors.white,blurRadius: 5,offset: const Offset(5,6))]
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white,
+                              blurRadius: 5,
+                              offset: const Offset(5, 6),
+                            ),
+                          ],
                         ),
                       ),
                       Container(
                         height: 150,
                         width: 150,
-                        foregroundDecoration:const  BoxDecoration(
+                        foregroundDecoration: const BoxDecoration(
                           shape: BoxShape.circle,
-                          //color: Colors.white
+                          // color: Colors.white
                           backgroundBlendMode: BlendMode.overlay,
                           gradient: LinearGradient(
-                              colors: [Colors.black,Colors.white]
+                            colors: [Colors.black, Colors.white],
                           ),
                         ),
                       ),
@@ -130,7 +123,7 @@ class WaterDropEffect extends StatelessWidget {
               ),
             ),
             // END
-            const  Spacer(),
+            const Spacer(),
           ],
         ),
       ),
