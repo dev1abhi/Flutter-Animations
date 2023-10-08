a8a45a88aa4e7f558e8524a9f4ce3ef45dd9c160
BlckDragonXabhi <abhilashsarangi222@gmail.com>
2023-10-08 17:48:08 +0530

    fix:reloading of bounce

--- lib/Screens/bounce_animation.dart
+++ lib/Screens/bounce_animation.dart
@@ -1,108 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/Screens/homepage.dart';
 import 'package:flutter_animations/controllers/drawercontroller.dart';
 import 'package:flutter_animations/helpers/colors.dart';
+import 'package:get/get.dart';
 
 class Bounce extends StatefulWidget {
   const Bounce({super.key});
+
+
 
   @override
   State<Bounce> createState() => _BounceState();
 }
 
 class _BounceState extends State<Bounce> {
+  bool shouldRefresh = false;
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( appBar: 
-       AppBar( actions: [IconButton(onPressed: () {
-         setState(() {
-            Navigator.pushReplacement(context, PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) => Bounce(),));
-         });
-       }, icon: Icon(Icons.refresh_rounded))],
-        backgroundColor: mainpagecolor,
-        title: const Text('bounce animation'),
-    leading: IconButton(
-    icon: const Icon(Icons.menu), // You can use any icon you prefer
-    onPressed:() {
-      MyDrawerController.to.toggleDrawer();
-      MyDrawerController.to.update();
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: () {
 
-    },
-    hoverColor: Colors.white,
-    ),
-    elevation: 0,
+              setState(() {
+                shouldRefresh = !shouldRefresh;
+              });
+
+
+          },
+
+              icon: Icon(Icons.refresh_rounded))
+          ],
+          backgroundColor: mainpagecolor,
+          title: const Text('bounce animation'),
+
+
+          leading: IconButton(
+            icon: const Icon(Icons.menu), // You can use any icon you prefer
+            onPressed: () {
+              MyDrawerController.to.toggleDrawer();
+              MyDrawerController.to.update();
+            },
+            hoverColor: Colors.white,
+          ),
+          elevation: 0,
         ),
-       body: Center( 
-         child: Column( 
-           mainAxisAlignment: MainAxisAlignment.center, 
-           children: [ 
-              // bounceInOut
-             TweenAnimationBuilder( 
-               child: CircleAvatar( 
-                 radius: 60, 
-                 backgroundColor: Colors.greenAccent, 
-                ), 
-                duration: Duration(milliseconds: 6000), 
-                curve: Curves.bounceInOut, 
-                tween: Tween(begin: 1.0, end: 0.0), 
-                builder: (context, value, child){
-                // horizontal disposition of the widget. 
-                 return Transform.translate( 
-                     offset:  Offset( 
-                       value * 500, 
-                        0.0 
-                     ), 
-                     child: child, 
-                   ); 
-                 }, 
-               ), 
-               SizedBox(height: 20,),
+        body: Center(
+            key: Key(shouldRefresh.toString()),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // bounceInOut
+                  TweenAnimationBuilder(
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.greenAccent,
+                    ),
+                    duration: Duration(milliseconds: 6000),
+                    curve: Curves.bounceInOut,
+                    tween: Tween(begin: 1.0, end: 0.0),
+                    builder: (context, value, child) {
+                      // horizontal disposition of the widget.
+                      return Transform.translate(
+                        offset: Offset(
+                            value * 500,
+                            0.0
+                        ),
+                        child: child,
+                      );
+                    },
+                  ),
+                  SizedBox(height: 20,),
 
-               //bounceOut
+                  //bounceOut
 
 
-                TweenAnimationBuilder( 
-               child: CircleAvatar( 
-                 radius: 60, 
-                 backgroundColor: Colors.redAccent, 
-                ), 
-                duration: Duration(milliseconds: 6000), 
-                curve: Curves.bounceOut, 
-                tween: Tween(begin: 1.0, end: 0.0), 
-                builder: (context, value, child){
-               
-                 return Transform.translate( 
-                     offset:  Offset( 
-                       value * 500, 
-                        0.0 
-                     ), 
-                     child: child, 
-                   ); 
-                 }, 
-               ), 
-               SizedBox(height: 20,),
+                  TweenAnimationBuilder(
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.redAccent,
+                    ),
+                    duration: Duration(milliseconds: 6000),
+                    curve: Curves.bounceOut,
+                    tween: Tween(begin: 1.0, end: 0.0),
+                    builder: (context, value, child) {
+                      return Transform.translate(
+                        offset: Offset(
+                            value * 500,
+                            0.0
+                        ),
+                        child: child,
+                      );
+                    },
+                  ),
+                  SizedBox(height: 20,),
 
-                TweenAnimationBuilder( 
-               child: CircleAvatar( 
-                 radius: 60, 
-                 backgroundColor: Colors.yellowAccent, 
-                ), 
-                duration: Duration(milliseconds: 6000), 
-                curve: Curves.bounceIn, 
-                tween: Tween(begin: 1.0, end: 0.0), 
-                builder: (context, value, child){
-                // horizontal disposition of the widget. 
-                 return Transform.translate( 
-                     offset:  Offset( 
-                       value * 500, 
-                        0.0 
-                     ), 
-                     child: child, 
-                   ); 
-                 }, 
-               ), 
-               SizedBox(height: 20,),
-           ]
-           )  )
-               );
+                  TweenAnimationBuilder(
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.yellowAccent,
+                    ),
+                    duration: Duration(milliseconds: 6000),
+                    curve: Curves.bounceIn,
+                    tween: Tween(begin: 1.0, end: 0.0),
+                    builder: (context, value, child) {
+                      // horizontal disposition of the widget.
+                      return Transform.translate(
+                        offset: Offset(
+                            value * 500,
+                            0.0
+                        ),
+                        child: child,
+                      );
+                    },
+                  ),
+                  SizedBox(height: 20,),
+
+                ]
+            ))
+    );
+
+
+
+
+
   }
+
 }
+
+
+
+
