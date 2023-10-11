31bff7e3fdff1966190b8c8bfa9874804cd0e253
habeebputhiyakath <irfanputhiyakat@gmail.com>
2023-10-11 23:25:19 +0530

    add app bar and small changes

--- lib/Screens/flip_counter.dart
+++ lib/Screens/flip_counter.dart
@@ -1,5 +1,7 @@
 import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/Controllers/drawercontroller.dart';
+import 'package:flutter_animations/helpers/colors.dart';
 
 
 class  flip extends StatefulWidget {
@@ -8,14 +10,41 @@ class  flip extends StatefulWidget {
 }
 
 class _flipState extends State<flip> {
+   
   double _value = 0.0;
 
   @override
   Widget build(BuildContext context) {
+    
+     bool shouldRefresh = false;
     return Scaffold(
       appBar: AppBar(
-        title: Text("FlipCounter "),
-      ),
+          actions: [
+            IconButton(onPressed: () {
+
+              setState(() {
+                shouldRefresh = !shouldRefresh;
+              });
+
+
+          },
+
+              icon: const Icon(Icons.refresh_rounded))
+          ],
+          backgroundColor: mainpagecolor,
+          title: const Text('bounce animation'),
+
+
+          leading: IconButton(
+            icon: const Icon(Icons.menu), 
+            onPressed: () {
+              MyDrawerController.to.toggleDrawer();
+              MyDrawerController.to.update();
+            },
+            hoverColor: Colors.white,
+          ),
+          elevation: 0,
+        ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -31,24 +60,24 @@ class _flipState extends State<flip> {
             ),
             AnimatedFlipCounter(
               value: _value,
-              duration: Duration(seconds: 2),
-              padding: EdgeInsets.symmetric(vertical: 8),
+              duration: const Duration(seconds: 2),
+              padding: const EdgeInsets.symmetric(vertical: 8),
               curve: Curves.elasticOut,
               textStyle: TextStyle(fontSize: 60, color: (_value > 0 ? Colors.green : Colors.red)),
             ),
             AnimatedFlipCounter(
               value: _value,
-              duration: Duration(seconds: 1),
+              duration: const Duration(seconds: 1),
               curve: Curves.bounceOut,
               wholeDigits: 4,
               fractionDigits: 2,
-              textStyle: TextStyle(fontSize: 40, color: Colors.blue),
+              textStyle: const TextStyle(fontSize: 40, color: Colors.blue),
             ),
             AnimatedFlipCounter(
               value: _value,
               prefix: "Level ",
-              padding: EdgeInsets.all(8),
-              textStyle: TextStyle(
+              padding: const EdgeInsets.all(8),
+              textStyle: const TextStyle(
                 fontSize: 80,
                 fontWeight: FontWeight.bold,
                 letterSpacing: -8.0,
