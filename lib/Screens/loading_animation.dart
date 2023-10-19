f92ab13dc421236e30264472e65ef9db2d62b86e
redvey <roopam12cse@gmail.com>
2023-10-19 22:01:17 +0530

    #Hacktober Fest 2023 1. Added New Animation: Loading Animation->Basically, it displays a circular loading animation(currently set to 86%).
    2. FIXED: While working, I found an issue in the menu section, the last 2 options had no spaces between leading to bad user experience and functionality as they both were navigating to a same page. I fixed it by separating them , now it's all fine now.

--- lib/Screens/loading_animation.dart
+++ lib/Screens/loading_animation.dart
@@ -10,7 +10,7 @@ class loadingAnimation extends StatefulWidget {
 }
 
 class _loadingAnimation extends State<loadingAnimation> {
-  bool _visible = true;
+  bool visible = true;
 
   @override
   Widget build(BuildContext context) {
